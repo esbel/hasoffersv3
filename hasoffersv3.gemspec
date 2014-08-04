da8079d9f0fc8421baec8c932eeb965484a005b5
Maximilian Seifert <maximilian.cypher@gmail.com>
2014-08-04 15:17:11 +0200

    Rate limiting specs

--- hasoffersv3.gemspec
+++ hasoffersv3.gemspec
@@ -24,4 +24,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'rspec',   '~> 3.0.0'
   s.add_development_dependency "bundler", "~> 1.3"
   s.add_development_dependency "rake"
+  s.add_development_dependency 'timecop', '~> 0.7.1'
 end
--- spec/lib/base_spec.rb
+++ spec/lib/base_spec.rb
@@ -32,4 +32,79 @@ describe HasOffersV3::Base do
       end
     end
   end
+
+  describe ".rate_limited" do
+    before(:each) do
+      HasOffersV3::Base.stub(:sleep) { |seconds| Timecop.freeze(Time.now + seconds) }
+    end
+
+    context 'rate limiting is not configured' do
+      before(:each) do
+        HasOffersV3.configuration.rate_limit = nil
+        if HasOffersV3::Base.instance_variable_defined?(:@last_request_at)
+          HasOffersV3::Base.send(:remove_instance_variable, :@last_request_at)
+        end
+        Timecop.freeze
+      end
+
+      it 'executes the block without delay' do
+        current_time = Time.now
+        HasOffersV3::Base.send(:rate_limited) do
+          expect(Time.now).to eq(current_time)
+        end
+      end
+    end
+
+    context 'rate limiting is set to 2 reqs / 10 seconds' do
+      before(:each) do
+        HasOffersV3.configuration.rate_limit = 2.0/10.0
+        if HasOffersV3::Base.instance_variable_defined?(:@last_request_at)
+          HasOffersV3::Base.send(:remove_instance_variable, :@last_request_at)
+        end
+        Timecop.freeze
+      end
+
+      context 'two requests are started immediately after each other' do
+        it 'delays the second request appropriately' do
+          current_time = Time.now
+          HasOffersV3::Base.send(:rate_limited) do
+            expect(Time.now).to eq(current_time)
+          end
+          HasOffersV3::Base.send(:rate_limited) do
+            expect(Time.now).to eq(current_time + 5)
+          end
+        end
+      end
+
+      context 'the second request is started 2 seconds after the first one' do
+        it 'delays the second request appropriately' do
+          current_time = Time.now
+          HasOffersV3::Base.send(:rate_limited) do
+            expect(Time.now).to eq(current_time)
+          end
+          Timecop.freeze(Time.now + 2)
+          HasOffersV3::Base.send(:rate_limited) do
+            expect(Time.now).to eq(current_time + 5)
+          end
+        end
+      end
+
+      context 'the second request is started 6 seconds after the first one' do
+        it 'does not delay the second request' do
+          current_time = Time.now
+          HasOffersV3::Base.send(:rate_limited) do
+            expect(Time.now).to eq(current_time)
+          end
+          Timecop.freeze(Time.now + 6)
+          HasOffersV3::Base.send(:rate_limited) do
+            expect(Time.now).to eq(current_time + 6)
+          end
+        end
+      end
+
+      after(:each) do
+        HasOffersV3.configuration.rate_limit = nil
+      end
+    end
+  end
 end
